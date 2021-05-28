047ce9e8f1aea2f90bad8b843c74d7d813bac849
Nghinh-Tri <52157962+Nghinh-Tri@users.noreply.github.com>
2021-05-28 17:53:21 +0700

    export cv and template file

--- src/service/action/import-file/ImportAction.js
+++ src/service/action/import-file/ImportAction.js
@@ -2,9 +2,9 @@ import { Modal } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import axios from "axios";
 import { PROFILE } from "../../constant";
-import { API_URL } from "../../util/util";
+import { API_URL, getEmpID } from "../../util/util";
 import { registerSuccess } from "../user/LoginAction";
-
+const FileDownload = require('js-file-download');
 export const addFile = (fileList) => {
     var url = `${API_URL}/User/Import`;
     const formData = new FormData();
@@ -47,18 +47,43 @@ export const addFile = (fileList) => {
 
 export const downloadCVFile = (empID) => {
     var url = `${API_URL}/User/Export/${empID}`;
-    console.log(url)
     return (dispatch) => {
         axios.get(
             url,
-            { headers: { Authorization: `Bearer ${localStorage.getItem("token").replace(/"/g, "")}` }, }
+            {
+                headers: { Authorization: `Bearer ${localStorage.getItem("token").replace(/"/g, "")}` },
+                responseType: 'blob'
+            }
         ).then(res => {
-            console.log(res.data)
+            FileDownload(res.data, 'CV.xlsx')
+            dispatch(exportSuccess())
         }).catch(err => {
             console.log('err', err)
         })
     }
 }
+
+export const exportTemplate = (empID) => {
+    var url = `${API_URL}/User/template/${getEmpID()}`;
+    return (dispatch) => {
+        axios.get(
+            url,
+            {
+                headers: { Authorization: `Bearer ${localStorage.getItem("token").replace(/"/g, "")}` },
+                responseType: 'blob'
+            }
+        ).then(res => {
+            FileDownload(res.data, 'Template CV.xlsx')
+            dispatch(exportSuccess())
+        }).catch(err => {
+            console.log('err', err)
+        })
+    }
+}
+
+export const exportSuccess = () => {
+    return { type: true };
+};
 
 export const addFileSuccess = (isSuccessed) => {
     return { type: PROFILE.IMPORT, isSuccessed };
